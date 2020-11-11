1c4b6a3f67c14cda97344f923a9c126a535646ba
urgyen <imalyanov@gmail.com>
2020-11-11 17:54:16 -0600

    update example
    include checkbox to randomize widths mainly for ie11 that doesn't
    support resizing divs

--- example/index.js
+++ example/index.js
@@ -2,14 +2,14 @@
 
 import "./style.css"
 
-import React from "react"
+import React, {useState} from "react"
 import {render} from "react-dom"
 import Truncate from "../src/index"
 
 const range = (x, y) => Array.from({length: (y + 1) - x}, (_, i) => (x + i))
 const randomDigit = () => Math.floor(Math.random() * 10)
 
-const Cell = ({even}) => {
+const Cell = ({randomize, even}) => {
   const length = randomDigit()
   const description = even ?
     `This cell has custom title, class and tail length of ${length}` :
@@ -17,9 +17,10 @@ const Cell = ({even}) => {
 
   const title = even ? "This is a custom title" : undefined
   const className = even ? "even" : undefined
+  const style = randomize ? {width: `${Math.max(100, randomDigit() * 30)}px`} : null
 
   return (
-    <div className="cell">
+    <div className="cell" style={style}>
       <Truncate tailLength={length} title={title} className={className}>
         {description}
       </Truncate>
@@ -28,15 +29,25 @@ const Cell = ({even}) => {
 }
 
 const Table = () => {
+  const [randomize, setRandomize] = useState(false)
   const rows = 2000
 
   return (
     <div>
       <h1>{rows} rows of resizable cells</h1>
+      <label htmlFor="randomize">
+        <input
+          id="randomize"
+          type="checkbox"
+          checked={randomize}
+          onChange={_ => setRandomize(x => !x)}
+        />
+        randomize width
+      </label>
       {range(1, rows).map(i => (
         <div key={i}>
           <Cell />
-          <Cell even />
+          <Cell randomize={i > 1 && randomize} even />
         </div>
       ))}
     </div>
