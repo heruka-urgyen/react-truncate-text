ababc791edad84d3814aef39f8979c85ad13ab84
urgyen <imalyanov@gmail.com>
2020-11-18 19:49:29 -0600

    improve find on page for untruncated text
    find on page now finds untruncated text correctly;
    truncated text is searchable before and after ellipsis

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import React, {useMemo} from "react"
+import React, {useMemo, useState, useRef, useEffect} from "react"
 import PropTypes from "prop-types"
+import useResizeObserver from "use-resize-observer"
 
 const containerStyle = {
   display: "flex",
@@ -18,23 +19,46 @@ const tailStyle = {
   whiteSpace: "pre",
 }
 
-const Truncate = ({children = "", tailLength, className, title = children}) => {
-  const [init, tail] = useMemo(() => {
+const sum = xs => xs.reduce((x, y) => x + y, 0)
+
+const Truncate = ({children = "", tailLength = 0, className, title = children}) => {
+  const [initRef, tailRef] = [useRef(), useRef()]
+  const [initEl, tailEl] = [initRef.current, tailRef.current]
+  const [text, textInit, textTail] = useMemo(() => {
     const text = children.toString()
 
-    if (tailLength > 0) {
-      return [text.slice(0, -tailLength), text.slice(-tailLength)]
-    }
-
-    return [text, ""]
+    return [text, text.slice(0, -tailLength), text.slice(-tailLength)]
   }, [children, tailLength])
+  const [[init, tail], setState] = useState([text, ""])
+  const [textWidth, setTextWidth] = useState(0)
+  const {ref, width} = useResizeObserver()
+
+  useEffect(() => {
+    if (initEl && tailEl) {
+      setTextWidth(sum([initEl, tailEl].map(x => x.scrollWidth)))
+    }
+  }, [initEl, tailEl])
+
+  useEffect(() => {
+    setTimeout(() => {
+      const isTruncated = width < textWidth
+
+      if (tailLength === 0 || (tail !== "" && !isTruncated)) {
+        setState([text, ""])
+      }
+
+      if (tailLength > 0 && tail === "" && isTruncated) {
+        setState([textInit, textTail])
+      }
+    }, 0)
+  }, [width, textWidth])
 
   return (
-    <div className={className} style={containerStyle} title={title}>
-      <span style={initStyle}>
+    <div ref={ref} className={className} style={containerStyle} title={title}>
+      <span ref={initRef} style={initStyle}>
         {init}
       </span>
-      <span style={tailStyle}>
+      <span ref={tailRef} style={tailStyle}>
         {tail}
       </span>
     </div>
