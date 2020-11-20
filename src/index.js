02402fdbc85cd2ca11a144f4ac40fbe3a0a35d1f
urgyen <imalyanov@gmail.com>
2020-11-19 23:21:18 -0600

    rewrite truncate component
    + change implementation from using flexbox and text-overflow to custom
    truncation based on the parent element heigth

--- src/index.js
+++ src/index.js
@@ -2,30 +2,28 @@ import React, {useMemo, useState, useRef, useEffect} from "react"
 import PropTypes from "prop-types"
 import useResizeObserver from "use-resize-observer/polyfilled"
 
+import "./style.css"
+
 const containerStyle = {
-  display: "flex",
-  width: "100%",
-  alignItems: "center",
-}
-
-const initStyle = {
   overflow: "hidden",
-  minWidth: 0,
-  textOverflow: "ellipsis",
+  textOverflow: "clip",
   whiteSpace: "pre",
 }
 
-const tailStyle = {
-  whiteSpace: "pre",
+const midStyle = {
+  fontSize: 0,
+  opacity: 0,
+}
+
+const getInitStyle = tailLength => tailLength > 0 ? {} : {
+  display: "inline-block",
+  overflow: "hidden",
+  textOverflow: "ellipsis",
+  whiteSpace: "nowrap",
+  width: "100%",
 }
 
 const sum = xs => xs.reduce((x, y) => x + y, 0)
-const onCopy = e => {
-  e.preventDefault()
-
-  const text = window.getSelection().toString()
-  e.clipboardData.setData("text", text.replace("\n", ""))
-}
 
 const Truncate = ({children = "", tailLength = 0, className, title = children}) => {
   const [initRef, tailRef] = [useRef(), useRef()]
@@ -33,40 +31,52 @@ const Truncate = ({children = "", tailLength = 0, className, title = children}) 
   const [text, textInit, textTail] = useMemo(() => {
     const text = children.toString()
 
-    return [text, text.slice(0, -tailLength), text.slice(-tailLength)]
+    if (tailLength > 0) {
+      return [text, text.slice(0, -tailLength), text.slice(-tailLength)]
+    }
+
+    return [text, text, ""]
   }, [children, tailLength])
-  const [[init, tail], setState] = useState([text, ""])
-  const [textWidth, setTextWidth] = useState(0)
+
+  const [[init, middle], setText] = useState([textInit, "", textTail])
+  const [[textWidth, charWidth], setTextWidth] = useState([0, 0])
+
   const {ref, width} = useResizeObserver()
+  const isTruncated = width < textWidth
 
   useEffect(() => {
-    if (initEl && tailEl) {
-      setTextWidth(sum([initEl, tailEl].map(x => x.scrollWidth)))
+    if (tailLength > 0 && initEl && tailEl) {
+      const offsetWidth = sum([initEl, tailEl].map(x => x.offsetWidth))
+      const charWidth = Math.ceil(offsetWidth / text.length)
+
+      setTextWidth([offsetWidth, charWidth])
     }
   }, [initEl, tailEl])
 
   useEffect(() => {
-    setTimeout(() => {
-      const isTruncated = width < textWidth
+    if (tailLength > 0) {
+      if (isTruncated) {
+        const charLen = Math.floor((width - charWidth * tailLength) / charWidth) - 3
+        const n = Math.max(1, charLen)
 
-      if (tailLength === 0 || (tail !== "" && !isTruncated)) {
-        setState([text, ""])
+        setText([textInit.slice(0, n), textInit.slice(n)])
+      } else {
+        setText([textInit, ""])
       }
-
-      if (tailLength > 0 && tail === "" && isTruncated) {
-        setState([textInit, textTail])
-      }
-    }, 0)
+    }
   }, [width, textWidth])
 
   return (
-    <div ref={ref} className={className} style={containerStyle} title={title} onCopy={onCopy}>
-      <span ref={initRef} style={initStyle}>
+    <div ref={ref} className={className} style={containerStyle} title={title}>
+      <span
+        ref={initRef}
+        style={getInitStyle(tailLength)}
+        className={isTruncated ? "react-truncate-text-truncated" : null}
+      >
         {init}
       </span>
-      <span ref={tailRef} style={tailStyle}>
-        {tail}
-      </span>
+      <span style={midStyle}>{middle}</span>
+      <span ref={tailRef}>{textTail}</span>
     </div>
   )
 }
