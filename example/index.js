205931bdedd5f993dd6224d6e1fb644fb47c5a64
urgyen <imalyanov@gmail.com>
2020-11-25 23:45:32 -0600

    update example

--- example/index.js
+++ example/index.js
@@ -4,7 +4,7 @@ import "./style.css"
 
 import React, {useMemo, useState} from "react"
 import {render} from "react-dom"
-import {VariableSizeGrid as Grid} from "react-window"
+import debounceRender from "react-debounce-render"
 import Truncate from "../src/index"
 
 const range = (x, y) => Array.from({length: (y + 1) - x}, (_, i) => (x + i))
@@ -15,67 +15,10 @@ const chars = range("1".charCodeAt(0), "z".charCodeAt(0)).map(x => String.fromCh
 const randomString = (length = 1) =>
   range(1, length).map(_ => chars[randomNumber(chars.length)]).join("")
 
-const Cell = randomize => ({data, rowIndex, columnIndex, style}) => {
-  const {tailLength, width, oddValue, evenValue} = data[rowIndex + columnIndex]
-  const odd = columnIndex % 2 === 1
-  const value = odd ? evenValue : oddValue
-  const title = odd ? "This is a custom title" : undefined
-  const className = "styled-truncate"
-  const customStyle = odd && randomize ? {width: `${width}px`} : undefined
-
-  return (
-    <div className="cell" style={{...style, ...customStyle}}>
-      <Truncate tailLength={tailLength} title={title} className={className}>
-        {value}
-      </Truncate>
-    </div>
-  )
-}
-
-const Rows = () => {
-  const rows = 2000
-  const [randomize, setRandomize] = useState(true)
-  const itemData = useMemo(() => range(1, rows + 1).map(_ => {
-    const length = randomDigit()
-
-    return {
-      tailLength: length,
-      width: Math.max(100, randomDigit() * 30),
-      evenValue: `${randomString(50)}${length}`,
-      oddValue: `${randomString(1).repeat(50)}${length}`,
-    }
-  }), [])
-
-  return (
-    <div>
-      <h1>{rows} rows of resizable cells</h1>
-      <label htmlFor="randomize" className="randomize-toggle">
-        <input
-          id="randomize"
-          type="checkbox"
-          checked={randomize}
-          onChange={_ => setRandomize(x => !x)}
-        />
-        randomize width
-      </label>
-      <Grid
-        itemData={itemData}
-        columnCount={2}
-        columnWidth={i => i % 2 === 0 ? 220 : 360}
-        height={window.innerHeight - 300}
-        rowCount={rows}
-        rowHeight={() => 40}
-        width={800}
-      >
-        {Cell(randomize)}
-      </Grid>
-    </div>
-  )
-}
-
-const Controlled = () => {
+const Controlled = React.memo(() => {
   const initialState = "mmmmmmmmmiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiimmmmmm"
   const [value, setValue] = useState(initialState)
+  const [width, setWidth] = useState(300)
   const [tailLength, setTailLength] = useState(randomDigit())
 
   return (
@@ -95,7 +38,7 @@ const Controlled = () => {
           onChange={e => setTailLength(parseInt(e.target.value, 10) || 0)}
         />
       </div>
-      <div className="controlled-truncate-container">
+      <div style={{width: `${width}px`}} className="controlled-truncate-container">
         <Truncate
           tailLength={tailLength}
           className="controlled-truncate styled-truncate"
@@ -103,18 +46,114 @@ const Controlled = () => {
           {value}
         </Truncate>
       </div>
+      <input
+        type="range"
+        min="0"
+        max="300"
+        value={width}
+        style={{width: "100%"}}
+        onChange={e => {
+          setWidth(e.target.value)
+        }}
+      />
+    </div>
+  )
+})
+
+const Rows = debounceRender(({widths, rows}) => {
+  const data = useMemo(() => range(1, rows), [rows])
+  const itemData = useMemo(() => range(1, rows + 1).map(_ => {
+    const length = randomDigit() || 1
+
+    return {
+      tailLength: length,
+      width: Math.max(100, randomDigit() * 30),
+      evenValue: `${randomString(50)}${length}`,
+      oddValue: `${randomString(1).repeat(50)}${length}`,
+    }
+  }), [rows])
+
+  const className = "styled-truncate"
+
+  return (
+    <div>
+      <table>
+        <tbody>
+          {data.map(i => (
+            <tr className="row" key={i}>
+              <td
+                className="col"
+                style={{
+                  maxWidth: `${widths[0]}px`,
+                  minWidth: `${widths[0]}px`,
+                }}
+              >
+                <Truncate tailLength={itemData[i].tailLength} className={className}>
+                  {itemData[i].evenValue}
+                </Truncate>
+              </td>
+              <td
+                className="col"
+                style={{
+                  maxWidth: `${widths[1]}px`,
+                  minWidth: `${widths[1]}px`,
+                }}
+              >
+                <Truncate tailLength={itemData[i].tailLength} className={className}>
+                  {itemData[i].oddValue}
+                </Truncate>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}, 100, {maxWait: 500})
+
+const Table = () => {
+  const rows = 2000
+  const [v1, setV1] = useState(200)
+  const [v2, setV2] = useState(200)
+
+  return (
+    <div>
+      <h1>table with {rows} rows <span style={{fontSize: "1rem"}}>(debounced)</span></h1>
+      <div>
+        <div style={{textAlign: "center", display: "inline-block", width: "200px"}}>
+          <input
+            type="range"
+            min="50"
+            max="200"
+            value={v1}
+            style={{width: "100%"}}
+            onChange={e => setV1(e.target.value)}
+          />
+        </div>
+        <div style={{textAlign: "center", display: "inline-block", width: "200px"}}>
+          <input
+            type="range"
+            min="50"
+            max="200"
+            value={v2}
+            style={{width: "100%"}}
+            onChange={e => setV2(e.target.value)}
+          />
+        </div>
+      </div>
+      <Rows widths={[v1, v2]} rows={rows} />
     </div>
   )
 }
 
-const Table = () => (
+const App = () => (
   <div>
     <Controlled />
-    <Rows />
+    <Table />
   </div>
 )
 
 render(
-  <Table />,
+  <App />,
   document.querySelector(".container"),
 )
